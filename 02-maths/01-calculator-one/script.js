61560bbeb43f408f33f9195fa964159731ab29e1
MeloScav <meloscavezzoni@gmail.com>
2022-10-04 13:41:30 +0200

    init

---
+++ 02-maths/01-calculator-one/script.js
@@ -0,0 +1,14 @@
+/* becode/javascript
+ *
+ * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+
+})();
