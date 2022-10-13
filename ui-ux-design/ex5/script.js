540bae18423ad1efef6023b2123dd2a217e74a5d
MeloScav <meloscavezzoni@gmail.com>
2022-10-13 15:13:58 +0200

    Ex 5

--- ui-ux-design/ex5/script.js
+++ ui-ux-design/ex5/script.js
@@ -1,11 +1,11 @@
-let numberArray = [5, 8, 9, 85, 2, 1854894168, 618, 1, 42 , 54894645];
+let numberArray = [5, 8, 9, 85, 2, 1854894168, 618, 1, 42, 54894645];
 
 let pairNumber = numberArray.filter(nb => nb % 2 === 0);
-console.log(`Le tableau contenant les chiffres pair est ${pairNumber}`);
+console.log("Le tableau contenant les chiffres pair est", pairNumber);
 
-const addNumberArrayInArray = (array, newArray) => {
-    let newNumberArray = array.concat(newArray);
-    return newNumberArray;
-}
+// const addNumberArrayInArray = (array, newArray) => {
+//     let newNumberArray = array.concat(newArray);
+//     return newNumberArray;
+// }
 
-console.log(`Le nouveau tableau constitué est ${addNumberArrayInArray(numberArray,pairNumber)}`);
+console.log("Le nouveau tableau constitué est", numberArray.concat(pairNumber));
