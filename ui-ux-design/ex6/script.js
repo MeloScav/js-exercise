31cac8f777aa9ce674f8763c6ad8c39b9cad7c45
MeloScav <meloscavezzoni@gmail.com>
2022-10-13 15:14:10 +0200

    Ex

---
+++ ui-ux-design/ex6/script.js
@@ -0,0 +1,6 @@
+const numberArray = [5, 8, 9, 85, 2, 1854894168, 618, 1, 42, 54894645];
+
+const numberArrayReversed = [...numberArray].reverse();
+
+console.log(numberArrayReversed);
+console.log(numberArray);
