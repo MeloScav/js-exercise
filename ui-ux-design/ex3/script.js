b8edee0c7c5cec1afd9c4ae93ca4ccff7f6e819c
MeloScav <meloscavezzoni@gmail.com>
2022-10-13 15:13:48 +0200

    Ex3

--- ui-ux-design/ex3/script.js
+++ ui-ux-design/ex3/script.js
@@ -1,4 +1,4 @@
-const numberArray = [5, 8, 9, 85, 618,1, 42 , 54894645];
-let x = 42;
+const numberArray = [5, 8, 9, 85, 618, 1, 42, 54894645];
+const x = 42;
 
-console.log(numberArray.indexOf(42,0));
+console.log(numberArray.indexOf(x, 0));
