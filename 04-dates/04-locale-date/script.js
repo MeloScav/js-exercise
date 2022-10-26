b02908ab687eaab905602b545f1c6dc349136b6d
MeloScav <meloscavezzoni@gmail.com>
2022-10-26 13:02:23 +0200

    Ex 4 : local date

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -10,5 +10,28 @@
 // You will have time to focus on it later.
 
 (() => {
+    const getLocalDate = (language) => {
+        const date = new Date();
+        const option = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+
+        return date.toLocaleDateString(language, option);
+    }
+
+    const getLocalTime = (hourSeparator) => {
+        const date = new Date();
+        const getHours = date.getHours();
+        const getMins = date.getMinutes();
+
+        const hoursToString = getHours.toString().padStart(2, "0");
+        const minsToString = getMins.toString().padStart(2, "0");
+
+        return hoursToString + hourSeparator + minsToString;
+    }
+
+    const showMessage = () => document.querySelector("#target").innerHTML = getLocalDate("fr-BE") + ", " + getLocalTime("h");
+
+    showMessage();
+    setInterval(() => showMessage(), 1000);
+
 
 })();
